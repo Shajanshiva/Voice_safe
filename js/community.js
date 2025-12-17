5f563a392128f35639c2371f4cf7f79e045a8e25
Shajanshiva <shajanshiva28@gmail.com>
2025-12-17 16:03:01 +0530

    js change in community

--- js/community.js
+++ js/community.js
@@ -17,21 +17,30 @@ function loadIssues() {
         storyDiv.className = "story";
 
         storyDiv.innerHTML = `
-          <div class="story-header">
-            <span class="tag">${issue.category_name}</span>
-            <span class="time">Anonymous</span>
-          </div>
+        <div class="story-header">
+            <span class="tag yellow">${issue.category_name}</span>
+            <span class="tag light">🔥 Trending</span>
+            <span class="time">2 hours ago</span>
+        </div>
 
-          <h3>${issue.title}</h3>
+        <h3>${issue.title}</h3>
 
-          <p>${issue.description.substring(0, 200)}...</p>
+        <p>${issue.description.substring(0, 2000)}...</p>
 
-          <div class="story-footer">
+        <div class="tags">
+            <span>#workplace</span>
+            <span>#anonymous</span>
+            <span>#support</span>
+        </div>
+
+        <div class="story-footer">
             <div class="actions">
-              <span>👍 Support</span>
+            <span>👍 0</span>
+            <span>💬 0</span>
+            <span>🤝 Support</span>
             </div>
             <button class="btn-read">Read More</button>
-          </div>
+        </div>
         `;
 
         container.appendChild(storyDiv);
