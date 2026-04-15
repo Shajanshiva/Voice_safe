882015813a7fffe381a89e213104427043642570
Shajanshiva <shajanshiva28@gmail.com>
2026-04-15 18:19:16 +0530

    changed constant.js

--- js/constant.js
+++ js/constant.js
@@ -1,3 +1,2 @@
-const API_URL = "https://voice-safe-backend-a1p3.vercel.app";
-const DUMMY = "";
+const API_URL = "https://fullstack-backend-xzvh.onrender.com";
 // const API_URL = "http://localhost:8000";
